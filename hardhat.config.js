8faa3fbc89b822818b4dfdea517c66d6a47858cf
Faran Ahmad <fakhan.cs@gmail.com>
2024-02-23 13:05:42 +0000

    changes to config

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,12 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+    },
+  },
   solidity: "0.8.24",
 };
