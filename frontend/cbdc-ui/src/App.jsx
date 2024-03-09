7b93d9e1476ddcf698375f280e412157448397bd
Faran Ahmad <fakhan.cs@gmail.com>
2024-03-09 01:37:21 +0000

    frontend boilerplate

---
+++ frontend/cbdc-ui/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h2>CBDC Token System</h2>
+    </div>
+  );
+}
+
+export default App;
